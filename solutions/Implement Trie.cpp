aa4d1814afba0f24ced17cc115463be7ff93b871
Sai Sasank <sai.sasank.yadati@gmail.com>
2019-01-14 23:34:11 +0530

    Create Implement Trie.cpp

---
+++ solutions/Implement Trie.cpp
@@ -0,0 +1,68 @@
+class TrieNode:
+    
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+        
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        curr = self.root
+        
+        for char in word:
+            if char not in curr.children.keys():
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        
+        curr.is_word = True
+        
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        curr = self.root
+        
+        for char in word:
+            if char not in curr.children.keys():
+                return False
+            curr = curr.children[char]
+        
+        return curr.is_word
+        
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        curr = self.root
+        
+        for char in prefix:
+            if char not in curr.children.keys():
+                return False
+            curr = curr.children[char]
+        
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
